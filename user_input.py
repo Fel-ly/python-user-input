23461c5bf3e8c3d9b4573754463687ac3615c488
Fel-ly <kiokofpm@gmail.com>
2024-02-22 22:53:44 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Where do you reside? ")
+
+print("Thank you for responding!")
+print("Hello ",name,"you are ", age, "years old and live in ", location)
