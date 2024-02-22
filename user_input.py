2656343877b030ae9f235397a552807a0af8c6a0
Fel-ly <kiokofpm@gmail.com>
2024-02-22 23:13:29 +0300

    age var update

--- user_input.py
+++ user_input.py
@@ -1,6 +1,6 @@
 name = input("Enter your name: ")
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 location = input("Where do you reside? ")
 
 print("Thank you for responding!")
-print("Hello ",name,"you are ", age, "years old and live in ", location)
+print("Hello",name,"you are", age, "years of age and live in", location)
